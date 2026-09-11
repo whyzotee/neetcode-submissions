bc0f196532767b97608fae9bedc39a640b1adcc7
Chatnarint Boonsaeng <53619535+whyzotee@users.noreply.github.com>
2026-09-11 13:02:01 +0700

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        let s: String = s
+            .chars()
+            .filter(|c| c.is_alphabetic() || c.is_numeric())
+            .collect();
+
+        let s = s.to_lowercase();
+        let s_reverse: String = s.chars().rev().collect();
+
+        &s == &s_reverse
+    }
+}

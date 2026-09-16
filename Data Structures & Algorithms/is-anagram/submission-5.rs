e0696c09342225a9cfa48a8b083d8e6b5c477402
Chatnarint Boonsaeng <53619535+whyzotee@users.noreply.github.com>
2026-09-16 21:36:37 +0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-5.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len() {
+            return false;
+        }
+
+        let mut s_sort:Vec<u8> = s.into_bytes();
+        let mut t_sort:Vec<u8> = t.into_bytes();
+
+        s_sort.sort();
+        t_sort.sort();
+
+        s_sort == t_sort
+    }
+}
